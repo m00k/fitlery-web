05b52409204eb13d47aea3ac77539d306c7305f2
m00k <christian.bican@gmail.com>
2020-04-05 16:18:08 +0200

    style(app): install material ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const toolbar = getByText(/fitlery/i);
+  expect(toolbar).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
+import AppBar from '@material-ui/core/AppBar';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Timer from '@material-ui/icons/Timer';
 
 function App() {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <AppBar color="primary" position="static">
+          <Toolbar className="Toolbar">
+            <Timer className="Icon" fontSize="large"/>
+            <Typography
+              variant="h3"
+              color="inherit"
+            >
+              fitlery
+           </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 }
 
