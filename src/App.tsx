34c2b47ae97645ef88b6329315bfa78b7e6a9dca
m00k <christian.bican@gmail.com>
2020-04-07 20:56:06 +0200

    style(app component): wrap with paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { Paper } from '@material-ui/core';
 import React from 'react';
 import NavBar from './NavBar';
 import WorkoutList from './workout/WorkoutList';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    height: '100vh',
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <React.Fragment>
+    <Paper className={classes.paper}>
       <NavBar></NavBar>
       <WorkoutList></WorkoutList>
-    </React.Fragment>
+    </Paper>
   );
 }
 
