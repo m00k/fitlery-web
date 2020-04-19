24f3802035275479474be16de76f864ebe5ba472
m00k <christian.bican@gmail.com>
2020-04-19 21:54:32 +0200

    refactor(pie countdown): simplify

--- src/player/GoAvatar.tsx
+++ src/player/GoAvatar.tsx
@@ -9,15 +9,13 @@ const calcPoint = (r: number = 1, cx: number = 0, cy: number = 0, percent: numbe
   ];
 };
 
+// TODO: useMemo?
 function calcArc(pFrom: number, pTo: number, r: number = 1, cx: number = 0, cy: number = 0) {
   const calcPointR = calcPoint.bind(null, r, cx, cy);
   const [startX, startY] = calcPointR(pFrom);
   const [endX, endY] = calcPointR(pTo);
   const largeArcFlag = Math.round(pTo - pFrom);
   return {
-    r,
-    cx,
-    cy,
     startX,
     startY,
     endX,
@@ -31,8 +29,18 @@ const Background = () => {
   return <circle r="1" fill={theme.palette.background.paper} />;
 }
 
+interface ArcProps {
+  from: number;
+  to: number;
+  r?: number;
+  cx?: number;
+  cy?: number;
+  fill?: string;
+}
+
 const Arc = (props: any) => {
-  const {r, cx, cy, startX, startY, endX, endY, largeArcFlag, fill} = props;
+  const {from, to, r = 1, cx = 0, cy = 0, fill = 'currentColor'} = props;
+  const {startX, startY, endX, endY, largeArcFlag} = calcArc(from, to, r, cx, cy);
   const d = 
     `M ${startX} ${startY} ` +
     `A ${r} ${r} 0 ${largeArcFlag} 1 ${endX} ${endY} ` +
@@ -49,8 +57,6 @@ const Arc = (props: any) => {
 const GoAvatar = (props: any) => {
   const { percentDone } = props;
   const theme = useTheme();
-  const arc1 = calcArc(0, percentDone);
-  const arc2 = calcArc(percentDone, 1);
 
   return (
     <Box
@@ -69,11 +75,13 @@ const GoAvatar = (props: any) => {
           : <>
               <Arc
                 fill={theme.palette.primary.light}
-                {...arc1}
+                from={0}
+                to={percentDone}
               />
               <Arc
                 fill={theme.palette.background.paper}
-                {...arc2}
+                from={percentDone}
+                to={1}
               />
             </>
         }
