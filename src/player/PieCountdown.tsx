bcec11ce5123c96359281d059c340ce78c1b6100
m00k <christian.bican@gmail.com>
2020-04-20 05:42:40 +0200

    refactor(pie countdown): properly type props

--- src/player/PieCountdown.tsx
+++ src/player/PieCountdown.tsx
@@ -1,6 +1,6 @@
 import Box from '@material-ui/core/Box';
 import useTheme from '@material-ui/core/styles/useTheme';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 const calcPoint = (r: number = 1, cx: number = 0, cy: number = 0, percent: number) => {
   return [
@@ -9,38 +9,39 @@ const calcPoint = (r: number = 1, cx: number = 0, cy: number = 0, percent: numbe
   ];
 };
 
-// TODO: useMemo?
-function calcArc(pFrom: number, pTo: number, r: number = 1, cx: number = 0, cy: number = 0) {
+const calcArc: (...args: any) => D = (pFrom: number, pTo: number, r: number = 1, cx: number = 0, cy: number = 0) => {
   const calcPointR = calcPoint.bind(null, r, cx, cy);
   const [startX, startY] = calcPointR(pFrom);
   const [endX, endY] = calcPointR(pTo);
-  const largeArcFlag = Math.round(pTo - pFrom);
+  const largeArcFlag = Math.round(pTo - pFrom) as 0 | 1 ;
   return {
     startX,
     startY,
     endX,
     endY,
-    largeArcFlag
+    largeArcFlag,
   };
 }
 
-const Background = () => {
-  const theme = useTheme();
-  return <circle r="1" fill={theme.palette.background.paper} />;
+interface D {
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+  largeArcFlag: 0 | 1;
 }
 
-interface ArcProps {
+interface ArcProps extends React.SVGProps<SVGPathElement> {
   from: number;
   to: number;
   r?: number;
   cx?: number;
   cy?: number;
-  fill?: string;
 }
 
-const Arc = (props: any) => {
+const Arc = (props: ArcProps) => {
   const {from, to, r = 1, cx = 0, cy = 0, fill = 'currentColor'} = props;
-  const {startX, startY, endX, endY, largeArcFlag} = calcArc(from, to, r, cx, cy);
+  const {startX, startY, endX, endY, largeArcFlag} = useMemo(() => calcArc(from, to, r, cx, cy), [from, to, r, cx, cy]);
   const d = 
     `M ${startX} ${startY} ` +
     `A ${r} ${r} 0 ${largeArcFlag} 1 ${endX} ${endY} ` +
@@ -53,6 +54,11 @@ const Arc = (props: any) => {
     />
   );
 };
+
+const Background = () => {
+  const theme = useTheme();
+  return <circle r="1" fill={theme.palette.background.paper} />;
+}
 
 const PieCountdown = (props: any) => {
   const { percentDone } = props;
@@ -86,7 +92,6 @@ const PieCountdown = (props: any) => {
             </>
         }
       </svg>
-
     </Box>
   );
 };
