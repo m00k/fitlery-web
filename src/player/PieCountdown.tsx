f4cda488ed9a4076dbdf1e062f9ef0539ab581be
m00k <christian.bican@gmail.com>
2020-04-20 22:24:31 +0200

    feat(pie countdown): use clip path to create color inversion effect

--- src/player/PieCountdown.tsx
+++ src/player/PieCountdown.tsx
@@ -4,6 +4,40 @@ import React from 'react';
 import Arc from './Arc';
 
 
+interface BackgroundProps {
+  text: string;
+  bgcolor: string;
+  color: string;
+  style: {};
+}
+
+const Background = (props: BackgroundProps) => {
+  const { bgcolor, color, style, text } = props;
+  const theme = useTheme();
+
+  return (
+    <Box
+      bgcolor={bgcolor}
+      color={color}
+      fontSize={theme.typography.h2.fontSize}
+      fontWeight={theme.typography.fontWeightBold}
+      borderRadius='50%'
+      lineHeight='112px' // TODO: magic numbers 
+      width={112} // TODO: magic numbers 
+      height={112} // TODO: magic numbers 
+      textAlign='center'
+      bottom={theme.spacing(1)}
+      top={theme.spacing(1)}
+      left={theme.spacing(1)}
+      right={theme.spacing(1)}
+      position="absolute"
+      style={style} // TODO: webkit vendor prefix
+    >
+      {text}
+    </Box>
+  );
+}
+
 interface PieCountdownProps extends BoxProps {
   fractionDone: number;
 }
@@ -11,6 +45,20 @@ interface PieCountdownProps extends BoxProps {
 const PieCountdown = (props: PieCountdownProps) => {
   const { fractionDone } = props;
   const theme = useTheme();
+
+  const bgDoneProps: BackgroundProps = {
+    text: !fractionDone ? 'Ready' : 'Go!',
+    bgcolor: theme.palette.background.paper,
+    color: theme.palette.secondary.dark,
+    style: fractionDone > 0 ? { clipPath: "url(#clip-left)" } : {}, // TODO: webkit vendor prefix
+  };
+
+  const bgLeftProps: BackgroundProps = {
+    text: fractionDone === 1 ? 'Done' : 'Go!',
+    bgcolor: theme.palette.primary.light,
+    color: theme.palette.secondary.contrastText,
+    style: fractionDone < 1 ? { clipPath: "url(#clip-done)" } : {}, // TODO: webkit vendor prefix
+  };
 
   return (
     <Box // TODO (cb): box vs grid?
@@ -21,44 +69,8 @@ const PieCountdown = (props: PieCountdownProps) => {
       p={1}
       position="relative"
     >
-      <Box
-        bgcolor={theme.palette.primary.light}
-        color={theme.palette.secondary.contrastText}
-        fontSize={theme.typography.h2.fontSize}
-        fontWeight={theme.typography.fontWeightBold}
-        borderRadius='50%'
-        lineHeight='112px' // TODO: magic numbers 
-        width={112} // TODO: magic numbers 
-        height={112} // TODO: magic numbers 
-        textAlign='center'
-        bottom={theme.spacing(1)}
-        top={theme.spacing(1)}
-        left={theme.spacing(1)}
-        right={theme.spacing(1)}
-        position="absolute"
-        style={fractionDone < 1 ? { clipPath: "url(#clip-done)" } : {}} // TODO: webkit vendor prefix
-      >
-        {fractionDone === 1 ? 'Done' : 'Go!'}
-      </Box>
-      <Box
-        bgcolor={theme.palette.background.paper}
-        color={theme.palette.secondary.dark}
-        fontSize={theme.typography.h2.fontSize}
-        fontWeight={theme.typography.fontWeightBold}
-        borderRadius='50%'
-        lineHeight='112px' // TODO: magic numbers 
-        width={112} // TODO: magic numbers 
-        height={112} // TODO: magic numbers 
-        textAlign='center'
-        bottom={theme.spacing(1)}
-        top={theme.spacing(1)}
-        left={theme.spacing(1)}
-        right={theme.spacing(1)}
-        position="absolute"
-        style={fractionDone > 0 ? { clipPath: "url(#clip-left)" } : {}} // TODO: webkit vendor prefix
-      >
-        {!fractionDone ? 'Ready' : 'Go!'}
-      </Box>
+      <Background {...bgDoneProps} />
+      <Background {...bgLeftProps} />
       <svg
         width="112px" // TODO: magic numbers
         viewBox="-1 -1 2 2"
@@ -74,9 +86,9 @@ const PieCountdown = (props: PieCountdownProps) => {
         </clipPath>
         <clipPath id="clip-left">
           <Arc
-            cx={56}
-            cy={56}
-            r={56}
+            cx={56} // TODO: magic numbers
+            cy={56} // TODO: magic numbers
+            r={56} // TODO: magic numbers
             fStart={fractionDone}
             fEnd={1}
           />
