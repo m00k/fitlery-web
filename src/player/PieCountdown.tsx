6bf944485514c88822ef1a2019aa3d926ae67cc4
m00k <christian.bican@gmail.com>
2020-04-20 05:47:44 +0200

    refactor(pie countdown): create useArc hook

--- src/player/PieCountdown.tsx
+++ src/player/PieCountdown.tsx
@@ -9,18 +9,20 @@ const calcPoint = (r: number = 1, cx: number = 0, cy: number = 0, percent: numbe
   ];
 };
 
-const calcArc: (...args: any) => D = (pFrom: number, pTo: number, r: number = 1, cx: number = 0, cy: number = 0) => {
-  const calcPointR = calcPoint.bind(null, r, cx, cy);
-  const [startX, startY] = calcPointR(pFrom);
-  const [endX, endY] = calcPointR(pTo);
-  const largeArcFlag = Math.round(pTo - pFrom) as 0 | 1 ;
-  return {
-    startX,
-    startY,
-    endX,
-    endY,
-    largeArcFlag,
-  };
+const useArc: (...args: any) => D = (pFrom: number, pTo: number, r: number = 1, cx: number = 0, cy: number = 0) => {
+  return useMemo(() => {
+    const calcPointR = calcPoint.bind(null, r, cx, cy);
+    const [startX, startY] = calcPointR(pFrom);
+    const [endX, endY] = calcPointR(pTo);
+    const largeArcFlag = Math.round(pTo - pFrom) as 0 | 1;
+    return {
+      startX,
+      startY,
+      endX,
+      endY,
+      largeArcFlag,
+    };
+  }, [pFrom, pTo, r, cx, cy]);
 }
 
 interface D {
@@ -40,13 +42,13 @@ interface ArcProps extends React.SVGProps<SVGPathElement> {
 }
 
 const Arc = (props: ArcProps) => {
-  const {from, to, r = 1, cx = 0, cy = 0, fill = 'currentColor'} = props;
-  const {startX, startY, endX, endY, largeArcFlag} = useMemo(() => calcArc(from, to, r, cx, cy), [from, to, r, cx, cy]);
-  const d = 
+  const { from, to, r = 1, cx = 0, cy = 0, fill = 'currentColor' } = props;
+  const { startX, startY, endX, endY, largeArcFlag } = useArc(from, to, r, cx, cy);
+  const d =
     `M ${startX} ${startY} ` +
     `A ${r} ${r} 0 ${largeArcFlag} 1 ${endX} ${endY} ` +
     `L ${cx} ${cy}`
-  ;
+    ;
   return (
     <path
       fill={fill}
@@ -73,23 +75,23 @@ const PieCountdown = (props: any) => {
       p={1}
     >
       <svg
-        width="100%" 
+        width="100%"
         viewBox="-1 -1 2 2"
       >
         {!percentDone
-          ? <Background/>
+          ? <Background />
           : <>
-              <Arc
-                fill={theme.palette.primary.light}
-                from={0}
-                to={percentDone}
-              />
-              <Arc
-                fill={theme.palette.background.paper}
-                from={percentDone}
-                to={1}
-              />
-            </>
+            <Arc
+              fill={theme.palette.primary.light}
+              from={0}
+              to={percentDone}
+            />
+            <Arc
+              fill={theme.palette.background.paper}
+              from={percentDone}
+              to={1}
+            />
+          </>
         }
       </svg>
     </Box>
