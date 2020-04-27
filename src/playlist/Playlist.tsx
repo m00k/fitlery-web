61ae779406dd085eecf133c83e3feba514b65838
m00k <christian.bican@gmail.com>
2020-04-27 20:12:55 +0200

    style(playlist): hack list height, fixed player (wip)

--- src/playlist/Playlist.tsx
+++ src/playlist/Playlist.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import ExerciseList from '../exercise/ExerciseList';
 import Player from '../player/Player';
 
-// TODO:
-// player position fixed, height max-content
-// exercise list height set to remaining space
+// TODO: HACK
+// expose all those magic numbers via theme
 const Playlist = () => {
   const theme = useTheme();
+  const navHeight = (theme.overrides?.MuiBottomNavigation?.root as any).height;
+  const mainMargin = theme.spacing(.5);
+  const playerHeight = theme.spacing(27);
+  const marginTop = theme.spacing(.5);
+  const height = `calc(100vh - ${navHeight * 2}px - ${mainMargin * 2}px - ${playerHeight}px - ${marginTop}px)`;
+  const overflow = 'auto';
+  const style = { marginTop, height, overflow };
   return (
     <>
       <Player />
-      <ExerciseList style={{marginTop: theme.spacing(.5)}}/>
+      <ExerciseList style={style}/>
     </>
   );
 }
