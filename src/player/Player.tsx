3e32d4f9629e0ea8c00ee83cf2f45083e2766ad0
m00k <christian.bican@gmail.com>
2020-05-01 06:09:45 +0200

    refactor(player store): extract hook

--- src/player/Player.tsx
+++ src/player/Player.tsx
@@ -1,48 +1,19 @@
+import React from 'react';
 import useTheme from '@material-ui/core/styles/useTheme';
-import React, { useEffect } from 'react';
-import { useCountdownStore } from '../countdown/CountdownStoreProvider';
 import Grid from '../shared/Grid';
 import { workouts } from '../workout/data';
 import Banner from './Banner';
 import Controls from './Controls';
 import Playlist from './Playlist';
-import { PlaylistActionType, usePlaylistStore } from './PlaylistStoreProvider';
+import usePlayerStore from './usePlayerStore';
 
 
 const Player = () => {
   const theme = useTheme();
-  const [playlistState, playlistDispatch] = usePlaylistStore();
+  const [ state, dispatch ] = usePlayerStore();
+  const { countdownState, playlistState } = state;
   const { playState } = playlistState;
-  const [countdownState, countdownDispatch] = useCountdownStore();
   const { msLeft, msTotal } = countdownState;
-
-  // TODO (cb): hook, combined reducer, rtk, ... ?
-  useEffect(() => {
-    if(msLeft === 0) {
-      playlistDispatch.next();
-      countdownDispatch.reset();
-    }
-  }, [playlistDispatch, msLeft, countdownDispatch]);
-  const handleControlsAction = (action: PlaylistActionType) => {
-    switch (action) {
-      case 'play':
-        playlistDispatch.play();
-        countdownDispatch.start();
-        break;
-      case 'pause':
-        playlistDispatch.pause();
-        countdownDispatch.pause();
-        break;
-      case 'prev':
-        playlistDispatch.prev();
-        countdownDispatch.reset();
-        break;
-      case 'next':
-        playlistDispatch.next();
-        countdownDispatch.reset();
-        break;
-    }
-  }
 
   return (
     <>
@@ -63,7 +34,7 @@ const Player = () => {
         </Banner>
         <Controls
           playState={playState}
-          onClick={handleControlsAction}
+          onClick={dispatch}
         />
       </Grid>
       <Playlist></Playlist>
---
+++ src/player/usePlayerStore.ts
@@ -0,0 +1,47 @@
+import { Dispatch, useEffect } from 'react';
+import { CountdownState, useCountdownStore } from '../countdown/CountdownStoreProvider';
+import { PlaylistActionType, PlaylistState, usePlaylistStore } from './PlaylistStoreProvider';
+
+export interface PlayerState {
+  countdownState: CountdownState;
+  playlistState: PlaylistState;
+}
+
+const usePlayerStore = (): [PlayerState, Dispatch<PlaylistActionType>] => {
+  const [playlistState, playlistDispatch] = usePlaylistStore();
+  const [countdownState, countdownDispatch] = useCountdownStore();
+  const { msLeft } = countdownState;
+  const state = { countdownState, playlistState };
+
+  useEffect(() => {
+    if(msLeft === 0) {
+      playlistDispatch.next();
+      countdownDispatch.reset();
+    }
+  }, [msLeft, countdownDispatch, playlistDispatch]);
+
+  const dispatch = (action: PlaylistActionType) => {
+    switch (action) {
+      case 'play':
+        playlistDispatch.play();
+        countdownDispatch.start();
+        break;
+      case 'pause':
+        playlistDispatch.pause();
+        countdownDispatch.pause();
+        break;
+      case 'prev':
+        playlistDispatch.prev();
+        countdownDispatch.reset();
+        break;
+      case 'next':
+        playlistDispatch.next();
+        countdownDispatch.reset();
+        break;
+    }
+  }
+
+  return [state, dispatch];
+}
+
+export default usePlayerStore;
