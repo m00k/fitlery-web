3578486d18511710a2d13a5a88c183ae4aac27d0
m00k <christian.bican@gmail.com>
2020-05-04 06:42:52 +0200

    refactor(playlist state): give breaks names

--- src/playlist/store/state.ts
+++ src/playlist/store/state.ts
@@ -1,76 +1,15 @@
 const BREAK_MS = 3 * 1000;
 const WORK_MS = 6 * 1000;
 const exercises: PlaylistItemData[] = [
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Wall Sit',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Lunge',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Plank',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Reverse Crunch',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Shadow Boxing',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Side Plank',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Side Plank (Left)',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Side Plank (Right)',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Knee Strikes',
-    durationMs: WORK_MS,
-  },
-  {
-    ...createBreak(BREAK_MS)
-  },
-  { 
-    name: 'Knee High Running',
-    durationMs: WORK_MS,
-  },
+  ...createPair('Wall Sit'),
+  ...createPair('Lunge'),
+  ...createPair('Plank'),
+  ...createPair('Reverse Crunch'),
+  ...createPair('Shadow Boxing'),
+  ...createPair('Side Plank (Left)'),
+  ...createPair('Side Plank (Right)'),
+  ...createPair('Knee Strikes'),
+  ...createPair('Knee High Running'),
 ];
 
 const NOT_FOUND = -1;
@@ -88,14 +27,20 @@ export const initialState: PlaylistState = {
 }
 
 export interface PlaylistItemData {
-  name?: string;
+  name: string;
   durationMs: number;
   tags?: string[];
 }
 
-function createBreak(durationMs: number): PlaylistItemData {
-  return {
-    durationMs,
+function createPair(name: string): [PlaylistItemData, PlaylistItemData] {
+  const b = {
+    name: `next up: ${name}`,
+    durationMs: BREAK_MS,
     tags: ['break'],
   };
+  const w = {
+    name,
+    durationMs: WORK_MS,
+  };
+  return [b, w];
 }
