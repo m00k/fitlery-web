503d2faa901802875bc4d90c022e815af390d7b0
m00k <christian.bican@gmail.com>
2020-05-04 06:46:47 +0200

    refactor(playlist current item): simplify handling of break text

--- src/playlist/PlaylistItemCurrent.tsx
+++ src/playlist/PlaylistItemCurrent.tsx
@@ -2,30 +2,19 @@ import { Box, BoxProps, Typography } from '@material-ui/core';
 import useTheme from '@material-ui/core/styles/useTheme';
 import React from 'react';
 import { isBreak } from './PlaylistItem';
-import { PlaylistItemData, usePlaylistStore } from './store';
+import { NOT_FOUND, usePlaylistStore } from './store';
 
-const getNextItem = (items: PlaylistItemData[], currentItemIndex: number) => {
-  const nextItemIndex = items.findIndex((item, index) => index >= currentItemIndex && !isBreak(item));
-  return items[nextItemIndex];
-}
-
-const getText = (items: PlaylistItemData[], currentItemIndex: number) => {
-  const currentItem = items[currentItemIndex];
-  if (!currentItem) {
-    return 'get ready';
-  }
-  return isBreak(currentItem)
-    ? `next up: ${getNextItem(items, currentItemIndex).name}`
-    : currentItem.name
-  ; 
-}
 
 const useInnerProps = () => {
   const [playlistState] = usePlaylistStore();
   const { currentItemIndex, items } = playlistState;
   const currentItem = currentItemIndex > -1 ? items[currentItemIndex] : items[0];
-  const fontSize = isBreak(currentItem) ? "2rem" : "3rem";
-  const text = getText(items, currentItemIndex);
+  const fontSize = isBreak(currentItem)
+    ? "2rem"
+    : "3rem";
+  const text = currentItemIndex === NOT_FOUND
+    ? 'get ready'
+    : currentItem.name;
   const style: React.CSSProperties = {
     textAlign: "center",
     textTransform: "uppercase",
