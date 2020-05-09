af523b4ec0522520ae5462056397df7912d07357
m00k <christian.bican@gmail.com>
2020-05-09 14:50:51 +0200

    style(player): calulate responsive column/row sizes

--- src/player/useLayout.ts
+++ src/player/useLayout.ts
@@ -1,5 +1,6 @@
 import { useTheme, useMediaQuery } from '@material-ui/core';
 
+// TODO (cb): useMemo()
 const usePageMediaQuery = () => {
   const theme = useTheme();
   const [downsm, upsm, upmd] = [
@@ -8,19 +9,46 @@ const usePageMediaQuery = () => {
     theme.breakpoints.up('md')
   ];
   const mqs = [
-    { bp: downsm, matches: useMediaQuery(downsm) },
-    { bp: upsm, matches: useMediaQuery(upsm) },
-    { bp: upmd, matches: useMediaQuery(upmd) },
+    { mq: downsm, matches: useMediaQuery(downsm, {defaultMatches: true}) },
+    { mq: upsm, matches: useMediaQuery(upsm) },
+    { mq: upmd, matches: useMediaQuery(upmd) },
   ];
-  const mq = mqs.filter(mq => mq.matches).map(mq => mq.bp).slice(-1)[0] || downsm;
-  return [mq, downsm, upsm, upmd];
+  const active = mqs
+    .filter(mqi => mqi.matches)
+    .map(mqi => mqi.mq)
+    .slice(-1)
+    [0] || downsm;
+  console.log('################', 'mq', active);
+  return [active, downsm, upsm, upmd];
 };
 
-// TODO: responsive row heights
-// TODO: magic numbers => theme, you know the drill...
+const useRowHeights = () => {
+  const [mq, downsm, upsm, upmd] = usePageMediaQuery();
+  const theme = useTheme();
+  const text = {
+    [downsm]: theme.spacing(16),
+    [upsm]: theme.spacing(17),
+    [upmd]: theme.spacing(15),
+  }[mq];
+  const controls = {
+    [downsm]: theme.spacing(11),
+    [upsm]: theme.spacing(11),
+    [upmd]: theme.spacing(13),
+  }[mq];
+  const current = {
+    [downsm]: theme.spacing(15),
+    [upsm]: theme.spacing(15),
+    [upmd]: theme.spacing(15),
+  }[mq];
+  return [text, controls, current];
+};
+
 export const useLayout = () => {
   const theme = useTheme();
   const [mq, downsm, upsm, upmd] = usePageMediaQuery();
+  const [textHeight, controlsHeight, currentHeight] = useRowHeights();
+  const totalHeight = textHeight + controlsHeight + currentHeight;
+  const listHeight = `minmax(0, calc(100vh - ${theme.variables.navbar.height * 2}px - ${totalHeight}px))`;
 
   const rootStyles: {[key: string]: any} = {
     [downsm]: {
@@ -31,8 +59,8 @@ export const useLayout = () => {
         "current current current current current"
         "list list list list list"
       `,
-      gridTemplateColumns: "128px repeat(4, 1fr)",
-      gridTemplateRows: `128px 88px 120px calc(100vh - ${theme.variables.navbar.height * 2}px - 128px - 88px - 120px)`,
+      gridTemplateColumns: `${textHeight}px repeat(4, 1fr)`,
+      gridTemplateRows: `${textHeight}px ${controlsHeight}px ${currentHeight}px ${listHeight}`,
     },
     [upsm]: {
       display: "grid",
@@ -42,8 +70,8 @@ export const useLayout = () => {
         "current current current current current"
         "list list list list list"
       `,
-      gridTemplateColumns: "repeat(2, 112px) repeat(3, 1fr)",
-      gridTemplateRows: `136px 88px 120px calc(100vh - ${theme.variables.navbar.height * 2}px - 136px - 88px - 120px)`, // TODO: prevent < 0
+      gridTemplateColumns: `repeat(2, ${(textHeight + controlsHeight) / 2}px) repeat(3, 1fr)`,
+      gridTemplateRows: `${textHeight}px ${controlsHeight}px ${currentHeight}px ${listHeight}`, // TODO: prevent < 0
     },
     [upmd]: {
       display: "grid",
@@ -53,12 +81,10 @@ export const useLayout = () => {
         "avatar avatar avatar current current"
         "list list list list list"
       `,
-      gridTemplateColumns: "repeat(3, 115px) repeat(2, 1fr)",
-      gridTemplateRows: `120px 104px 120px calc(100vh - ${theme.variables.navbar.height * 2}px - 120px - 104px - 120px)`,
+      gridTemplateColumns: `repeat(3, ${totalHeight / 3}px) repeat(2, 1fr)`,
+      gridTemplateRows: `${textHeight}px ${controlsHeight}px ${currentHeight}px ${listHeight}`,
     }
   };
-
-  console.log('###################', mq);
 
   const root = rootStyles[mq];
   const avatar = {
