e795728200dfd8fc2ff8d8b2e82ae3d6f7d06d1e
m00k <christian.bican@gmail.com>
2020-05-24 16:43:23 +0200

    style(card): fix grid row issue for chrome

--- src/shared/card/Card.tsx
+++ src/shared/card/Card.tsx
@@ -12,6 +12,7 @@ const Card = ({ children, onClick }: PropsWithChildren<CardProps>) => {
       borderRadius={1}
       display='grid'
       gridColumn={['span 6', 'span 4', 'span 3']}
+      gridTemplateRows='5fr 2fr'
       boxShadow={8}
       onClick={onClick}
     >
