06da7c3ecd886f994b3dabd6becd4017cbd9fdbd
m00k <christian.bican@gmail.com>
2020-08-08 14:33:21 +0200

    style(avatar): fix height for safari

--- src/shared/Avatar.tsx
+++ src/shared/Avatar.tsx
@@ -31,8 +31,6 @@ const Avatar: React.FC<AvatarProps> = ({ text, ...props }) => {
       {...root}
     >
       <svg
-        width="100%"
-        height="100%"
         viewBox={`0 0 ${VIEWBOX_SIZE} ${VIEWBOX_SIZE}`}
       >
         <circle
