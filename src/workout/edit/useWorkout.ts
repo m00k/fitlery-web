ad316b44caad2cfa5728e622a91266a1318b7fe1
m00k <christian.bican@gmail.com>
2021-06-26 04:51:41 +0200

    chore: update dependencies

--- src/workout/edit/useWorkout.ts
+++ src/workout/edit/useWorkout.ts
@@ -5,7 +5,7 @@ import { WorkoutData, workoutReducer, WorkoutState } from '../store';
 
 // find workout by id param and sync state
 const useWorkout = (workoutState: WorkoutState, setWorkoutState: SetterOrUpdater<WorkoutState>): WorkoutData | undefined => {
-  const { id } = useParams();
+  const { id } = useParams<{id: string}>();
   const { items: workouts } = workoutState;
   const foundAt = workouts.findIndex(w => w.id === id);
   if (foundAt > -1 && workoutState.currentItemIndex !== foundAt) {
